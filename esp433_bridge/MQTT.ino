cba4a9f3ec66ece3b5af0a8bd516fc69f1a1e206
AlessandroAU <alessandroaus@gmail.com>
2017-02-04 22:15:02 +1000

    More fixes again

--- esp433_bridge/MQTT.ino
+++ esp433_bridge/MQTT.ino
@@ -108,25 +108,4 @@ void HandleMQTT() {
   }
 }
 
-//
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
